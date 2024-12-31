e2fbbe5da777c8773339541b6b5bc389bc632824
Meg <marinaadasilva3456@gmail.com>
2024-12-30 22:06:47 -0300

    Adicionei novas funcionalidades

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,56 @@
+let contador = 0;
 let entrada = document.querySelector(".entrada");
 let botaoAdicionar = document.querySelector(".bi-plus");
 let main = document.getElementById("minha-lista");
+const limparTudo = document.querySelector(".bi-trash2-fill");
+let minhaLista = document.getElementById("minha-lista");
 
-// Função para adicionar tarefa
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcar(id) {
+  var marcarLista = document.getElementById(id);
+  if (marcarLista.style.textDecoration === "line-through") {
+    marcarLista.style.textDecoration = "none";
+  } else {
+    marcarLista.style.textDecoration = "line-through";
+  }
+}
+
+limparTudo.addEventListener("click", function () {
+  minhaLista.remove();
+});
+
 botaoAdicionar.addEventListener("click", function () {
   let valorEntrada = entrada.value.trim();
 
+  ++contador;
   if (valorEntrada !== "") {
     let novaTarefa = document.createElement("div");
+    novaTarefa.id = `${contador}`;
     novaTarefa.classList.add("lista");
-
- 
 
     novaTarefa.innerHTML = `
       <div class="lista-checkbox">
-        <input type="checkbox" />
+        <input onclick="marcar(${contador})" type="checkbox" />
       </div>
       <div class="lista-descricao">${valorEntrada}</div>
       <div class="lista-lixeira">
-        <i class="bi bi-trash-fill"></i>
+        <i onclick="deletar(${contador})" class="bi bi-trash-fill"></i>
       </div>
     `;
 
     main.appendChild(novaTarefa);
 
-
     entrada.value = "";
     console.log("botao adicionado");
   }
+
+  input.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      botaoAdicionar.click();
+    }
+  });
 });
